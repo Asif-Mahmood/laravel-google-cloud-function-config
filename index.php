33cc3a4891e2005f6b5c618739686489ab554d62
Asif Mahmood <asif.mahmood@gmail.com>
2022-05-27 18:10:05 +0500

    change function name to laravelapi

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use Psr\Http\Message\ServerRequestInterface;
 
-function laravel(ServerRequestInterface $request): string
+function laravelapi(ServerRequestInterface $request): string
 {
     require_once __DIR__.'/public/index.php';
 
